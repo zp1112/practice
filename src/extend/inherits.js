98b0c133bb71ccca21c6df7eb76b26ac4db535d7
Candy <you@example.com>
2018-05-25 15:24:22 +0800

    a

--- src/extend/inherits.js
+++ src/extend/inherits.js
@@ -5,15 +5,24 @@ function Person1(name) {
   this.super(name)
   // Person.call(this, name);
 }
-Person1.prototype = Object.create(Person.prototype, {
-  constructor: {
-    value: Person1,
-    enumerable: false,
-    writable: true,
-    configurable: true
-  }
-}, { super: Person })
-console.log(111, Person1)
+// Person1.prototype = Object.create(Person.prototype, {
+//   constructor: {
+//     value: Person1,
+//     enumerable: false,
+//     writable: true,
+//     configurable: true
+//   },
+//   super: {
+//     value: Person,
+//     enumerable: false,
+//     writable: true,
+//     configurable: true
+//   }
+// })
+Person1.prototype = Object.assign(Person.prototype, {
+  constructor: Person1,
+  super: Person,
+})
 // Person1.prototype.super = Person;
 let person1 = new Person1('candy');
 console.log(person1.name);
